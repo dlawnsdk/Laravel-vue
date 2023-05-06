aff533ee5231ff09c9e5ce0518b0184ab8d0d046
Juna Im <dlawnsdk04@naver.com>
2023-05-06 14:11:03 +0900

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+require('./bootstrap');
+import { createApp } from 'vue'
+import HelloWorld from '../components/Test.vue';
+
+const app = createApp({});
+app.component('hello-world', HelloWorld)
+    .mount('#app');
+
+require('./bootstrap');
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<body class="antialiased">
+<div id="app">
+    <hello-world/>
+</div>
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
